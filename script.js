5564ce462e210b5ec6e72c7e1ed4c2c814be3a30
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-02-26 12:06:20 -0700

    woot canvas painting

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let canvas=document.getElementById(`game`);
+let ctx= canvas.getContext("2d");
 
+canvas.width = 600;
+canvas.hight = 600;
+let background = new Image();
+background.src = "images/space.png";
+
+function game (){
+ctx.drawImage(background,0,0,canvas.width,canvas.hight)
+}
+setInterval(game,1000/60)
