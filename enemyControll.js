eb3a932ef75be70df06a8a64f84bce55c08e0802
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-02-26 12:30:02 -0700

    push on enemys

---
+++ enemyControll.js
@@ -0,0 +1,19 @@
+export default class enemyControll{
+   enemyMap=[
+    [1,1,1,1,1]
+    [1,1,1,1,1]
+    [2,2,3,2,2]
+    [2,2,3,2,2]
+    [1,1,1,1,1]
+    [2,2,2,2,2]
+
+   ]
+
+
+    constructor(canvas){
+        this.canvas = canvas;
+    }
+    draw(ctx) {
+
+    }
+}
--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let canvas=document.getElementById(`game`);
 let ctx= canvas.getContext("2d");
+import enemyControll from "./enemyControll.js";
 
 canvas.width = 600;
 canvas.hight = 600;
 let background = new Image();
 background.src = "images/space.png";
-
+let EnemyControll = new enemyControll(canvas)
 function game (){
 ctx.drawImage(background,0,0,canvas.width,canvas.hight)
+EnemyControll.draw(ctx)
 }
 setInterval(game,1000/60)
+
+//enemy controller
+
